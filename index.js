50ab6c75df3b83989b2fafc9f5f20343da299696
qinchao888 <2164109314@qq.com>
2020-01-11 19:42:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+document.designMode = 'on';
+document.body.contentEditable = true;
+((tags) => {
+  function remove (tag) {
+    const listeners = window.getEventListeners(tag);
+    if (Object.keys(listeners).length) {
+      for (let eventName in listeners) {
+        listeners[eventName].forEach((item, index) => tag.removeEventListener(eventName, item.listener))
+      }
+    }
+  }
+  remove(document);
+  (Object.getOwnPropertyNames(Object.getPrototypeOf(Object.getPrototypeOf(document)))).filter(name => name.indexOf('on') === 0).forEach(name => {
+    document[name] = null;
+    tags.forEach(tag => {
+      remove(tag)
+      tag[name] = null;
+    })
+  })
+})(Array.from(document.getElementsByTagName('*')));
